9c2d126a79fefc8f7fa312da2f79304b45a330a7
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-08 21:39:26 +0530

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+
+const myRoute:Routes = [
+  {
+    path:"",
+    component:ViewCourseComponent
+  },
+  {
+    path:"add",
+    component:AddCourseComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCourseComponent,
+    ViewCourseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-course/view-course.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-course',
+  templateUrl: './view-course.component.html',
+  styleUrls: ['./view-course.component.css']
+})
+export class ViewCourseComponent {
+  constructor(private api:ApiService) {
+    api.fetchCourses().subscribe(
+      (response)=>{
+        this.courses = response
+      }
+    )
+  }
+  courses:any = []
+}
